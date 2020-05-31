78139e95e0812cf47d36c85091f316c9b6df22d7
wilbur8831 <35630754+wilbur8831@users.noreply.github.com>
2020-05-31 21:15:02 +0800

    老二讚

--- Baseline/src/module hazard_process.v
+++ Baseline/src/module hazard_process.v
@@ -5,14 +5,13 @@ module hazard_process(
     input branch_flag;
     output stall_pc;
     output flush_IF/ID;
-
     /*
     beq:{000,1100011}	
     bne:{001,1100011}
     lw:{010,0000011}
     */ 
-
 );
+
 always@(*) begin
     if(ID/EX_memread && (ID/EX_rt == IF/ID_rs1 || ID/EX_rt == IF/ID_rs2) ) begin 
         stall_pc = 1'b1;
@@ -23,7 +22,7 @@ always@(*) begin
         flush_IF/ID = 1'b0;
     end
     if (branch_flag) begin 
-        stall_pc = 1'b1;
+        stall_pc = 1'b0;
         flush_IF/ID = 1'b1;
     end
     else begin
