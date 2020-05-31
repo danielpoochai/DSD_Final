c3d230f36e2a6890500c51262b6187206eeb44c1
wilbur8831 <35630754+wilbur8831@users.noreply.github.com>
2020-05-31 19:58:02 +0800

    1

--- Baseline/src/module forwarding_unit.v
+++ Baseline/src/module forwarding_unit.v
@@ -1,8 +1,8 @@
 module forwarding_unit(
-    input [31:0] ID/EX_rs1;
-    input [31:0] ID/EX_rs2;
-    input [31:0] EX/MEM_rd;
-    input [31:0] MEM/WB_rd;
+    input [4:0] ID/EX_rs1;
+    input [4:0] ID/EX_rs2;
+    input [4:0] EX/MEM_rd;
+    input [4:0] MEM/WB_rd;
     input EX/MEM_regwrite;
     input MEM/WB_regwrite;
     output [1:0] EX/MEM_rs1_control;
@@ -10,9 +10,18 @@ module forwarding_unit(
 );
 
 always@(*) begin
-    if()
-
-
+    if(EX/MEM_regwrite && (EX/MEM_rd != 5'd0) && (EX/MEM_rd == ID/EX_rs1)) begin
+        EX/MEM_rs1_control = 2'b10;
+    end
+    else if (MEM/WB_regwrite && MEM/WB_rd != 32'd0 && !(EX/MEM_regwrite && EX/MEM_rd != 5'd0 && EX/MEM_rd == ID/EX_rs1) && MEM/WB_rd == ID/EX_rs1  )
+        EX/MEM_rs1_control = 2'b01;
+    else EX/MEM_rs1_control = 2'b00;
+    if(EX/MEM_regwrite && EX/MEM_rd != 5'd0 && EX/MEM_rd == ID/EX_rs2) begin
+        EX/MEM_rs2_control = 2'b10;
+    end
+    else if (MEM/WB_regwrite && MEM/WB_rd != 32'd0 && !(EX/MEM_regwrite && EX/MEM_rd != 5'd0 && EX/MEM_rd == ID/EX_rs2) && MEM/WB_rd == ID/EX_rs2  )
+        EX/MEM_rs2_control = 2'b01;
+    else EX/MEM_rs2_control = 2'b00;
 
 
 end
--- Baseline/src/module hazard_process.v
+++ Baseline/src/module hazard_process.v
@@ -1,3 +1,33 @@
 module hazard_process(
-    
-)
+    input [4:0] ID/EX_rt;
+    input [4:0] IF/ID_rs1,IF/ID_rs2;
+    input  ID/EX_memread;
+    input branch_flag;
+    output stall_pc;
+    output flush_IF/ID;
+
+    /*
+    beq:{000,1100011}	
+    bne:{001,1100011}
+    lw:{010,0000011}
+    */ 
+
+);
+always@(*) begin
+    if(ID/EX_memread && (ID/EX_rt == IF/ID_rs1 || ID/EX_rt == IF/ID_rs2) ) begin 
+        stall_pc = 1'b1;
+        flush_IF/ID = 1'b1;
+    end
+    else begin 
+        stall_pc = 1'b0;
+        flush_IF/ID = 1'b0;
+    end
+    if (branch_flag) begin 
+        stall_pc = 1'b1;
+        flush_IF/ID = 1'b1;
+    end
+    else begin
+        stall_pc = 1'b0;
+        flush_IF/ID = 1'b0;
+    end
+end
