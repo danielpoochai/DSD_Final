6f4f0734972a29e4832de338223d15db521afda1
wilbur8831 <35630754+wilbur8831@users.noreply.github.com>
2020-05-31 16:46:03 +0800

    Update module forwarding_unit.v

--- Baseline/src/module forwarding_unit.v
+++ Baseline/src/module forwarding_unit.v
@@ -1,3 +1,18 @@
 module forwarding_unit(
-    
-)
+    input [31:0] ID/EX_rs1;
+    input [31:0] ID/EX_rs2;
+    input [31:0] EX/MEM_rd;
+    input [31:0] MEM/WB_rd;
+    input EX/MEM_regwrite;
+    input MEM/WB_regwrite;
+    output [1:0] EX/MEM_rs1_control;
+    output [1:0] EX/MEM_rs2_control;
+);
+
+always@(*) begin
+    if()
+
+
+
+
+end
