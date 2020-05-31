c0b994cd4d48bd46d63c69cb1d8fb083faeb6de9
wilbur8831 <35630754+wilbur8831@users.noreply.github.com>
2020-05-31 23:15:51 +0800

    w

--- Baseline/src/module hazard_process.v
+++ Baseline/src/module hazard_process.v
@@ -1,10 +1,13 @@
 module hazard_process(
     input [4:0] ID/EX_rt;
     input [4:0] IF/ID_rs1,IF/ID_rs2;
+    input [4:0] EX/MEM_rt;
+    input EX/MEM_memread;
     input  ID/EX_memread;
     input branch_flag;
-    output stall_pc;
-    output flush_IF/ID;
+    output hazard_stall;
+    output hazard_flush;
+    output stall_lwbranch;
     /*
     beq:{000,1100011}	
     bne:{001,1100011}
@@ -14,19 +17,29 @@ module hazard_process(
 
 always@(*) begin
     if(ID/EX_memread && (ID/EX_rt == IF/ID_rs1 || ID/EX_rt == IF/ID_rs2) ) begin 
-        stall_pc = 1'b1;
-        flush_IF/ID = 1'b1;
+        hazard_stall = 1'b1;
+        hazard_flush = 1'b1;
+        stall_lwbranch = 1'b0;
     end
     else begin 
-        stall_pc = 1'b0;
-        flush_IF/ID = 1'b0;
+        hazard_stall = 1'b0;
+        hazard_flush = 1'b0;
+        stall_lwbranch = 1'b0;
     end
     if (branch_flag) begin 
-        stall_pc = 1'b0;
-        flush_IF/ID = 1'b1;
+        hazard_stall = 1'b0;
+        hazard_flush = 1'b1;
+        stall_lwbranch = 1'b0;
     end
     else begin
-        stall_pc = 1'b0;
-        flush_IF/ID = 1'b0;
+        hazard_stall = 1'b0;
+        hazard_flush = 1'b0;
+        stall_lwbranch = 1'b0;
     end
+    if(EX/MEM_memread && (EX/MEM_rt == IF/ID_rs1 || EX/MEM_rt == IF/ID_rs2) &&  ) begin
+        
+
+
+    end
+    
 end
